3aa94fc919e8b0d89a12c494af296a593010db3a
DavidBistricean <103377308+DavidBistricean@users.noreply.github.com>
2022-04-11 20:08:36 -0600

    Create mario-more.c

---
+++ Problem-sets/mario-more.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_int(const char *prompt) 
+{
+		const int buffer_size = 256;
+		printf("%s", prompt);
+		char input[buffer_size];
+		fgets(&input, buffer_size, stdin);
+		fflush(stdin); 
+		return atoi(input);
+}
+
+void print_whitespace(int amount) 
+{
+	while (amount > 0) 
+	{
+		printf(" ");
+		amount = amount - 1;
+	}
+}
+
+void print_steps(int amount) 
+{
+	while (amount > 0) 
+	{
+		printf("#");
+		amount = amount - 1;
+	}
+	//printf("\n");
+}
+
+int main(void) 
+{
+  int height = 0;
+  while (1) 
+	{
+		height = get_int("Height: ");
+		if (height >= 1 && height <= 8) 
+		{
+			break;
+		} 
+		else 
+		{
+			continue; 
+		}
+	}
+
+	int steps = 0; 
+	int size = height;
+	while (height >= 0) 
+	{
+		print_whitespace(size - steps);
+		print_steps(steps);
+		printf(" "); 
+		print_steps(steps);
+		printf("\n"); 
+		steps = steps + 1;
+		height = height - 1;
+	}
+  return 0;
+}
